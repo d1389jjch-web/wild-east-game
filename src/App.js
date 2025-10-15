71de31c34b34b1071c3d89f1c0d760f364af77e8
d1389jjch-web <d1389jjch@gmail.com>
2025-10-15 01:19:44 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WildEastGame from '../WildEastGame';
+
+function App() {
+  return (
+    <div className="App">
+      <WildEastGame />
+    </div>
+  );
+}
+
+export default App;
